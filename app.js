63a2c4db808bcfba312ace3e8284acabded35e84
Ivan Zapata <falenyo@gmail.com>
2020-10-12 23:30:41 -0400

    trying routes

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+const birds = require('./birds')
 
-app.get('/example/b', function (req, res, next) {
-    console.log('the response will be sent by the next function ...')
-    next()
-  }, function (req, res) {
-    res.send('Hello from B!')
-  })
+app.use('/', birds);
 
 app.listen(port, () => {
-    console.log(`This app is listening at http://localhost:${port}`)
+  console.log(`This app is listening at http://localhost:${port}`);
 });
