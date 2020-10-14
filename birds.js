77dabbfd5736a6dfcdedf676be066d7c61196492
Ivan J. Zapata-Rivera <59859202+johnsbrook@users.noreply.github.com>
2020-10-13 23:43:24 -0400

    Update birds.js
    Updated code

--- birds.js
+++ birds.js
@@ -5,11 +5,13 @@ var router = express.Router()
 router.use(function timeLog (req, res, next) {
   console.log('Time: ', Date.now())
   next()
-})
+});
+
 // define the home page route
 router.get('/', function (req, res) {
   res.send('Birds home page')
-})
+});
+
 // define the about route
 router.get('/about', function (req, res) {
   res.send('About birds')
