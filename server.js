5c7789f627689b8e4c05b44c6f0517379a1f1581
Ivan Zapata <falenyo@gmail.com>
2020-10-15 20:58:54 -0400

    re-structured site architecture

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const path = require("path");
+const nodemon = require('nodemon');
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("./assets/html"));
+
+const route = require('./routes/html-route');
+
+app.use('/', route);
+app.use('/engine', route);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {console.log(`App running on http://localhost:${PORT}`)})
