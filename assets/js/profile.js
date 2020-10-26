825e0c15cb385bc9b0ebc5508edf2ced2adc12ac
Ivan Zapata <falenyo@gmail.com>
2020-10-25 22:18:26 -0400

    added update status functionality OK

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,43 +1,45 @@
 function profile() {
   const profile = document.querySelector("#profilepic");
-  const profileapi = "https://photo-logger.herokuapp.com/profilepic";
+  const profileapi = "/profilepic";
 
   if (profile) {
     fetch(profileapi)
       .then((res) => res.json(profileapi))
       .then((d) => {
+        console.log(d);
         var dL = d.length - 1;
+        console.log(dL);
         var URL = d[dL].URL;
+        console.log(URL)
         var img = `<img src=${URL} class="profilepic">`;
         profile.insertAdjacentHTML("afterbegin", img);
       });
+
+    // This code allows to show and hide the form to post profile picture
+    const changePro = document.querySelector("#show");
+    const form = document.querySelector("#form");
+
+    changePro.addEventListener("click", function () {
+      if (form.style.display === "block") {
+        form.style.display = "none";
+      } else {
+        form.style.display = "block";
+      }
+      console.log(form.style.display);
+    });
+
+    // Thide code will allow to show and hide the form to post a status update
+    const changeStatus = document.querySelector("#showStatus");
+    const statusForm = document.querySelector("#formStatus");
+    changeStatus.addEventListener("click", function () {
+      if (statusForm.style.display === "block") {
+        statusForm.style.display = "none";
+      } else {
+        statusForm.style.display = "block";
+      }
+      console.log(statusForm.style.display);
+    });
   }
-
-  // This code allows to show and hide the form to post profile picture
-  const changePro = document.querySelector("#show");
-  const form = document.querySelector("#form");
-  changePro.addEventListener("click", function () {
-    if (form.style.display === "block") {
-      form.style.display = "none";
-    } else {
-      form.style.display = "block";
-    }
-    console.log(form.style.display);
-  });
-
-
-  // Thide code will allow to show and hide the form to post a status update
-  const changeStatus = document.querySelector('#showStatus');
-  const statusForm = document.querySelector('#formStatus')
-  changeStatus.addEventListener('click', function() {
-    if (statusForm.style.display === "block") {
-      statusForm.style.display = "none";
-    } else {
-      statusForm.style.display = "block";
-    }
-    console.log(statusForm.style.display);
-  })
-
 }
 
 export { profile };
--- assets/js/status.js
+++ assets/js/status.js
@@ -1,5 +1,21 @@
 function status() {
-    console.log('You\'re in status.js')
+    console.log('You\'re in status.js');
+    const status = document.querySelector('#statusText');
+    const statusAPI = "/photologstatus";
+
+    if(status) {
+        fetch(statusAPI)
+        .then((res) => res.json(statusAPI))
+        .then((d) => {
+            console.log(d);
+            var dL = d.length - 1;
+            console.log(dL);
+            var URL = d[dL].statusUpdate;
+            console.log(URL);
+            var update = `<p class="text-left">${URL}</p>`
+            status.insertAdjacentHTML("afterbegin", update);
+        })
+    }
 }
 
 export { status }
