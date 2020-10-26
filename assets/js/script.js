526b556c3d13ee936ddd39b40e98c3658f3c117e
Ivan Zapata <falenyo@gmail.com>
2020-10-25 21:55:16 -0400

    added status.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 import { Nav } from './navbar.js';
 import { logger } from './logger.js';
 import { profile } from './profile.js';
+import { status } from './status.js';
 
 
 Nav();
 logger();
 profile();
+status();
---
+++ assets/js/status.js
@@ -0,0 +1,5 @@
+function status() {
+    console.log('You\'re in status.js')
+}
+
+export { status }
