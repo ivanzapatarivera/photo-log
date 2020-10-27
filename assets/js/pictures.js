e69876e1505aec79d3ec434b14a117c2e4b4e958
Ivan Zapata <falenyo@gmail.com>
2020-10-26 23:09:34 -0400

    images cards posted

--- assets/js/pictures.js
+++ assets/js/pictures.js
@@ -1,5 +1,24 @@
 function pictures() {
     console.log('You\'re in pictures.')
+    
+    const event = document.querySelector('#picturesWall');
+    const API = ('/log')
+    if (event) {
+        fetch(API)
+        .then((res) => res.json(API))
+        .then((d) => {
+            console.log(d);
+            d.map((d) => {
+                const title = d.title;
+                const URL = d.URL;
+                var card = `<div class="card">
+                            <h5>${title}</h5>
+                            <img src=${URL} class="cardImage" />
+                            </div>`
+                event.insertAdjacentHTML("afterbegin", card);
+            })
+        })
+    }
 }
 
 export { pictures };
