432898737cf7232fbb41e7e3b43cab1039731c68
Ivan Zapata <falenyo@gmail.com>
2020-11-02 16:01:32 -0500

    eliminated createNode and appendNode for template literal

--- assets/js/status.js
+++ assets/js/status.js
@@ -1,14 +1,8 @@
 function status(event) {
-  // console.log("You're in status.js");
+ 
   const status = document.querySelector("#statusText");
   const previousStats = document.querySelector("#previousStatus");
   const statusAPI = "/photologstatus";
-  const createNode = (elem) => {
-    return document.createElement(elem);
-  };
-  const appendNode = (parent, elem) => {
-    parent.appendChild(elem);
-  };
 
   if (status) {
     previousStatus();
@@ -36,42 +30,15 @@ function status(event) {
 
             let statusText = d.statusUpdate;
 
-            let statusCard = `<div class="mt-3 col-12 col-md-12 mx-auto card text-left pt-2 px-4 pb-4" style="border-radius: 15px;" data-id="${id}">
-            <span style="font-size: .8rem;">Created on: ${timestamp} <i class="far fa-trash-alt text-right delete"></i> </span><br>
+            let statusCard = 
+            `<div class="mt-3 col-12 col-md-12 mx-auto card text-left pt-2 px-4 pb-4" style="border-radius: 15px;" data-id="${id}">
+            <span style="font-size: .8rem;">${timestamp} 
+            <span onClick="delete" class="delete"><i class="far fa-trash-alt text-right delete"></i>
+            </span></span><br>
             <span data-id="${id}">${statusText}</span></div>`;
 
             previousStats.insertAdjacentHTML("afterbegin", statusCard);
-            // let li = createNode("div");
-            // li.setAttribute(
-            //   "class",
-            //   "mt-3 col-12 col-md-12 mx-auto card text-left pt-2 px-4 pb-4"
-            // );
-            // li.setAttribute("style", "border-radius: 15px;");
-            // li.setAttribute("data-id", id);
-
-            // let icon = createNode("i");
-            // icon.setAttribute("class", "far fa-trash-alt text-right delete");
-
-            // spanDate.innerText =
-            //   "Created on: " +
-            //   dateArray[1] +
-            //   "/" +
-            //   dateArray2[0] +
-            //   "/" +
-            //   dateArray[0] +
-            //   " @" +
-            //   dateArray3[0];
-            // spanDate.setAttribute("style", "font-size: .8rem;");
-            // let dateID = spanDate + " " + id;
-            // let spanStatus = createNode("span");
-            // spanStatus.setAttribute("data-id", id)
-            // spanStatus.innerText = d.statusUpdate;
-            // appendNode(li, spanDate);
-            // appendNode(li, br);
-            // appendNode(li, br);
-            // appendNode(li, spanStatus);
-            // appendNode(li, icon);
-            // appendNode(previousStats, li);
+            
           });
         })
         .catch((err) => {
