9a834a72fdfaebff1679a13fc4b87d885a04794b
Ivan Zapata <falenyo@gmail.com>
2020-11-02 16:31:45 -0500

    formatted JS

--- routes/api.js
+++ routes/api.js
@@ -3,116 +3,114 @@ const mongoose = require("mongoose");
 const mongojs = require("mongojs");
 const logger = require("morgan");
 
-const logs = require('../models/logs.js');
-const profilepic = require('../models/profilepic.js');
-const statusUpdate = require('../models/statusupdate.js');
+const logs = require("../models/logs.js");
+const profilepic = require("../models/profilepic.js");
+const statusUpdate = require("../models/statusupdate.js");
 const app = express();
 
-
 // These are the routes for photo logs
-app.post('/log', ({ body }, res) => {
-    logs.create(body)
-    .then(db => {
-        console.log(db)
+app.post("/log", ({ body }, res) => {
+  logs
+    .create(body)
+    .then((db) => {
+      console.log(db);
     })
-    .then(db => {
-        res.redirect('/thankyou')
+    .then((db) => {
+      res.redirect("/thankyou");
     })
-    .then(db => {
-        res.redirect('/profile')
+    .then((db) => {
+      res.redirect("/profile");
     })
-    .catch(err => {
-        res.json(err);
+    .catch((err) => {
+      res.json(err);
     });
 });
 
-app.get('/log', (req, res) => {
-    logs.find({})
-    .then(cb => {
-        res.json(cb)
+app.get("/log", (req, res) => {
+  logs
+    .find({})
+    .then((cb) => {
+      res.json(cb);
     })
-    .catch(err => {
-        res.json(err);
+    .catch((err) => {
+      res.json(err);
     });
 });
-
 
 // These are the routes for profile picture updates
-app.post('/profilepic', ({ body }, res) => {
-    profilepic.create(body)
-    .then(db => {
-        console.log(db)
+app.post("/profilepic", ({ body }, res) => {
+  profilepic
+    .create(body)
+    .then((db) => {
+      console.log(db);
     })
-    .then(db => {
-        res.redirect('/profile')
+    .then((db) => {
+      res.redirect("/profile");
     })
-    .catch(err => {
-        res.json(err)
-    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-app.get('/profilepic', (req, res) => {
-    profilepic.find({})
-    .then(cb => {
-        res.json(cb)
+app.get("/profilepic", (req, res) => {
+  profilepic
+    .find({})
+    .then((cb) => {
+      res.json(cb);
     })
-    .catch(err => {
-        res.json(err);
-    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
-
 
 // These are the route for status updates
-app.post('/photologstatus', ({ body }, res) => {
-    statusUpdate.create(body)
-    .then(db => {
-        console.log(db)
+app.post("/photologstatus", ({ body }, res) => {
+  statusUpdate
+    .create(body)
+    .then((db) => {
+      console.log(db);
     })
-    .then(db => {
-        res.redirect('/profile')
+    .then((db) => {
+      res.redirect("/profile");
     })
-    .catch(err => {
-        res.json(err)
-    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
-app.get('/photologstatus', (req, res) => {
-    statusUpdate.find({}).sort({timestamp: -1})
-    .then(cb => {
-        res.json(cb)
+app.get("/photologstatus", (req, res) => {
+  statusUpdate
+    .find({})
+    .sort({ timestamp: -1 })
+    .then((cb) => {
+      res.json(cb);
     })
-    .catch(err => {
-        res.json(err);
-    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 app.get("/find/:id", (req, res) => {
-    statusUpdate.findOne(
-        {_id: mongojs.ObjectId(req.params.id)},
-        (err, data) => {
-            if (err) {
-                res.send(error);
-            }
-            else {
-                res.send(data)
-            }
-        }
-    )
+  statusUpdate.findOne(
+    { _id: mongojs.ObjectId(req.params.id) },
+    (err, data) => {
+      if (err) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    }
+  );
 });
 
 app.delete("/delete/:id", (req, res) => {
-    statusUpdate.remove(
-        {_id: mongojs.ObjectId(req.params.id)},
-        (err, data) => {
-            if (err) {
-                res.send(error);
-            }
-            else {
-                res.send(data)
-            }
-        }
-    )
+  statusUpdate.remove({ _id: mongojs.ObjectId(req.params.id) }, (err, data) => {
+    if (err) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
 });
-
 
 module.exports = app;
