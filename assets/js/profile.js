1d02ea5c3231fcec15aad34d524f8972a6a5b251
Ivan Zapata <falenyo@gmail.com>
2020-11-02 17:32:05 -0500

    formatted and refactored JS document

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -6,16 +6,12 @@ function profile() {
     fetch(profileapi)
       .then((res) => res.json(profileapi))
       .then((d) => {
-        // console.log(d);
         var dL = d.length - 1;
-        // console.log(dL);
         var URL = d[dL].URL;
-        // console.log(URL)
         var img = `<img src=${URL} class="profilepic mt-4">`;
         profile.insertAdjacentHTML("afterbegin", img);
       });
 
-    // This code allows to show and hide the form to post profile picture
     const changePro = document.querySelector("#show");
     const form = document.querySelector("#form");
 
@@ -25,7 +21,6 @@ function profile() {
       } else {
         form.style.display = "block";
       }
-      // console.log(form.style.display);
     });
   }
 }
