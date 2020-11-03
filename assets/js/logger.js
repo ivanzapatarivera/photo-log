87240632e481fa55c37c850f872a15ffd48657dc
Ivan Zapata <falenyo@gmail.com>
2020-11-02 22:06:45 -0500

    changed spacing of input and textarea

--- assets/js/logger.js
+++ assets/js/logger.js
@@ -6,13 +6,13 @@ function logger() {
                         <h5 class="text-center">Log your Photo!</h5>
                         <form action="/log" method="post">
                             <label for="title">Title</label><br>
-                            <input type="text" name="title" id="title" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;How would you like to name this picture?"><br>
+                            <input class="px-3 py-1" type="text" name="title" id="title" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;How would you like to name this picture?"><br>
                             <label for="URL">Web Address</label><br>
-                            <input type="text" name="URL" id="URL" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;What's the URL where this picture is located?"><br>
+                            <input class="px-3 py-1" type="text" name="URL" id="URL" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;What's the URL where this picture is located?"><br>
                             <label for="location">Location</label><br>
-                            <input type="text" name="location" id="location" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Where was this picture taken?"><br>
+                            <input class="px-3 py-1" type="text" name="location" id="location" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Where was this picture taken?"><br>
                             <label for="description">Description</label><br>
-                            <textarea type="text" name="description" id="description" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Would you like to say something about this picture?"></textarea><br>
+                            <textarea class="px-3 py-1" type="text" name="description" id="description" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Would you like to say something about this picture?"></textarea><br>
                             <button class="button btn btn-primary">Log</button>
                         </form>
                     </div>
