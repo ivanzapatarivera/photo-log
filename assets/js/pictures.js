8948c29d475bb2f0ce3b45bcac7ea9c93e4a997c
Ivan Zapata <falenyo@gmail.com>
2020-11-02 21:59:04 -0500

    added description to photos

--- assets/js/pictures.js
+++ assets/js/pictures.js
@@ -8,12 +8,14 @@ function pictures() {
         d.map((d) => {
           const title = d.title;
           const id = d._id;
-
+          const description = d.description;
           const URL = d.URL;
           var card = `<div class="card cards text-center">
                         <p class="cardTitle mt-4" data-id=${id}>${title} 
                         <span onClick="delete" data-id=${id} class="delete"><i class="far fa-trash-alt delete" data-id=${id}></i></span></p>
-                        <p><img src=${URL} class="cardImage" /><br></p> 
+                        <p><img src=${URL} class="cardImage" /><br>
+                        <span>${description}</span>
+                        </p> 
                         </div>`;
           event.insertAdjacentHTML("afterbegin", card);
         });
