dad08cdf7d164d44621419fe4932de13d4b5f474
Ivan Zapata <falenyo@gmail.com>
2020-11-05 11:17:13 -0500

    changed pictureWall between status and timeline

--- assets/js/pictures.js
+++ assets/js/pictures.js
@@ -10,11 +10,11 @@ function pictures() {
           const id = d._id;
           const description = d.description;
           const URL = d.URL;
-          var card = `<div class="card cards text-center">
-                        <p class="cardTitle mt-4" data-id=${id}>${title} 
+          var card = `<div class="card cards text-center col-2">
+                        <p class="cardTitle mt-4" data-id=${id}>
                         <span onClick="delete" data-id=${id} class="delete"><i class="far fa-trash-alt delete" data-id=${id}></i></span></p>
                         <p><img src=${URL} class="cardImage" /><br>
-                        <span>${description}</span>
+                        
                         </p> 
                         </div>`;
           event.insertAdjacentHTML("afterbegin", card);
