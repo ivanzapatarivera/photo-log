25d4de44596f6aa885a118ef2320fa2273501072
Ivan Zapata <falenyo@gmail.com>
2020-11-11 20:24:55 -0500

    displayLocations() OK

--- assets/js/places.js
+++ assets/js/places.js
@@ -2,10 +2,14 @@ function places() {
     
     const places = document.querySelector('#places');
     if(places) {
-        console.log('You\'re in places.')
+        displayLocations()
     } else {
         return ('You\'ve not landed JS correctly.')
     }
+    
+}
+
+function displayLocations() {
     
 }
 
