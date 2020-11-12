0ecb28b60108179507faef7a45ebd1b40346ca8d
Ivan Zapata <falenyo@gmail.com>
2020-11-12 18:42:48 -0500

    removed event listener

--- assets/js/places.js
+++ assets/js/places.js
@@ -11,14 +11,15 @@ function displayLocations() {
     .then((res) => res.json(API))
     .then((response) => {
       response.map((divResponse) => {
+
+        // Display name of album by each location
         var divLocations = `<div class="col-12 text-center">
                               <h5 id="${divResponse}" class="mt-4">${divResponse}</h5>
                             </div>`;
         placesEl.insertAdjacentHTML("afterend", divLocations);
-        var imageEl = document.getElementById(`${divResponse}`);
-        imageEl.addEventListener("click", function () {
-        });
 
+        // Display all images contained in each album
+        var imageEl = document.getElementById(`${divResponse}`);
         var locationsAPI = API + divResponse;
         fetch(locationsAPI)
           .then((res) => res.json())
