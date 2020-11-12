0277b02bc3c94fe01390da3e585ee1d65722973a
Ivan Zapata <falenyo@gmail.com>
2020-11-11 20:23:19 -0500

    places() import OK

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,11 @@ import { logger } from './logger.js';
 import { profile } from './profile.js';
 import { status } from './status.js';
 import { pictures } from './pictures.js';
-
+import { places } from './places.js';
 
 nav();
 logger();
 profile();
 status();
 pictures();
+places();
