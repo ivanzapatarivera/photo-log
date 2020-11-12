b7eb711d6ab6f737a5cbad8b39539605d6c4d1b0
Ivan Zapata <falenyo@gmail.com>
2020-11-12 17:56:01 -0500

    refactored code

--- assets/js/places.js
+++ assets/js/places.js
@@ -11,28 +11,21 @@ function displayLocations() {
     .then((res) => res.json(API))
     .then((response) => {
       response.map((divResponse) => {
-        console.log(divResponse);
         var divLocations = `<div class="col-12">
                               <h5 id="${divResponse}">${divResponse}</h5>
                             </div>`;
         placesEl.insertAdjacentHTML("afterend", divLocations);
         var imageEl = document.getElementById(`${divResponse}`);
         imageEl.addEventListener("click", function () {
-          console.log(`You've clicked on ${divResponse}.`);
         });
 
         var locationsAPI = API + divResponse;
-        console.log(locationsAPI);
-
         fetch(locationsAPI)
           .then((res) => res.json())
           .then((res) => {
-            // console.log(res.length);
             for (var i = 0; i < res.length; i++) {
               var URL = res[i].URL;
-              // console.log(`${divResponse} `, URL)
-              var divImage = `<a href="${URL}" target="_blank"> <img src=${URL} class="col-10 py-4" />`;
-              console.log(divImage);
+              var divImage = `<a href="${URL}" target="_blank"><img src=${URL} class="col-10 py-4" /></a>`;
               imageEl.insertAdjacentHTML("afterend", divImage);
             }
           });
