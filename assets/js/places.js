65734e5bafb7aaf488239a4fe78a278641e466b1
Ivan Zapata <falenyo@gmail.com>
2020-11-11 23:11:02 -0500

    console.log map by location OK

--- assets/js/places.js
+++ assets/js/places.js
@@ -1,5 +1,3 @@
-
-
 const placesEl = document.querySelector("#places");
 function places() {
   if (placesEl) {
@@ -7,32 +5,38 @@ function places() {
   }
 }
 
-const API = ('/getLocations/');
-var locationsArray = [];
+const API = "/getLocations/";
 function displayLocations() {
- 
-
-   fetch(API)
+  fetch(API)
     .then((res) => res.json(API))
     .then((response) => {
-        console.log(response)
-        response.map((locations) => {
-            console.log(locations);
-            locationsArray.push(locations)
-            var card = `<div class="text-center">
-                        <h6>${locations}</h6>
-                        </div>`
-            placesEl.insertAdjacentHTML("afterend", card)
+      response.map((locations) => {
+        console.log(locations);
+        var card = `<div class="text-center">
+                        <h6 id="${locations}">${locations}</h6 id="${locations}">
+                    </div>`;
+        placesEl.insertAdjacentHTML("afterend", card);
+
+        fetch(API + locations)
+        .then((res) => res.json(API + locations))
+        .then((allLocations) => {
+          console.log(allLocations);
+          
         })
-    })
 
-    allLocations();
+        
+    });
+
+      // for (var i = 0; i < response.length; i++) {
+      //   fetch(API + response[i])
+      //     .then((res) => res.json(API + response[i]))
+      //     .then((response) => {
+      //       response.map((locationsJSON) => {
+      //         console.log(response);
+      //       });
+      //     });
+      // }
+    });
 }
-
-function allLocations() {
-  console.log('These are all the locations: ', locationsArray)
-
-}
-
 
 export { places };
