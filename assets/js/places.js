ba587c7765045f7ed0eee017bf5ebad81af80961
Ivan Zapata <falenyo@gmail.com>
2020-11-12 16:58:19 -0500

    added event on dynamically created DOM

--- assets/js/places.js
+++ assets/js/places.js
@@ -9,28 +9,27 @@ const API = "/getLocations/";
 var allLocationsArray = [];
 function displayLocations() {
 
-  var locationsArray = [];
 
   fetch(API)
     .then((res) => res.json(API))
     .then((response) => {
       response.map((divResponse) => {
+        console.log(divResponse)
         var divLocations = `<div class="col-12 bg-danger">
                               <h5 id="${divResponse}">${divResponse}</h5>
                             </div>`;
         placesEl.insertAdjacentHTML("afterbegin", divLocations);
-        locationsArray.push(divResponse);
+        var imageEl = document.getElementById(`${divResponse}`);
+        imageEl.addEventListener("click", function() {
+          console.log(`You've clicked on ${divResponse}.`);
+        })
       })
+
+
+      
     })
-  console.log('This is locationsArray: ', locationsArray);
-  allLocationsArray.push(locationsArray)
-  displayPictures();
-}
 
-var locations
 
-function displayPictures() {
-  console.log('These is allLocationsArray inside displayPictures(): ' + allLocationsArray);
 }
 
 export { places };
