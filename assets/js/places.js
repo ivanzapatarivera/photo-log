bcf27fde256aa11bcb82fd7fd6380bc8869ba36a
Ivan Zapata <falenyo@gmail.com>
2020-11-12 23:49:11 -0500

    formatted JS

--- assets/js/places.js
+++ assets/js/places.js
@@ -10,10 +10,7 @@ function displayLocations() {
   fetch(API)
     .then((res) => res.json(API))
     .then((response) => {
-      console.log(response);
       response.map((divResponse) => {
-
-        
         // Display name of album by each location
         var divLocations = `<div class="col-12 text-center">
                               <h5 id="${divResponse}" class="mt-4 divResponse">${divResponse}</h5>
@@ -22,34 +19,44 @@ function displayLocations() {
 
         // Display all images contained in each album
         var imageEl = document.getElementById(`${divResponse}`);
-        var photoLogsBoxEl = document.getElementById('recentPhotosLogsBox');
-        var photoLogsImageCardsEl = document.getElementById('picturesWall');
+        var photoLogsBoxEl = document.getElementById("recentPhotosLogsBox");
+        var photoLogsImageCardsEl = document.getElementById("picturesWall");
         var locationsAPI = API + divResponse;
 
-        imageEl.addEventListener("click", function(){
-          // console.log(`You've clicked ${divResponse}.`);
+        imageEl.addEventListener("click", function () {
           photoLogsBoxEl.innerHTML = divResponse;
-          // console.log(`You've clicked on ${photoLogsImageCardsEl.innerHTML}.`);
           fetch(locationsAPI)
-          .then((res) => res.json())
-          .then((res) => {
-            console.log(res.length)
-            for (var i = 0; i < res.length; i++) {
-              var URL = res[i].URL;
-              // console.log(URL);
-              var divImage = `<a href="${URL}" target="_blank"><img src=${URL} class="col-10 col-md-3 my-2 divImage" /></a>`;
-              console.log(divImage);
-              emptyHTML();
-              function emptyHTML() {
-                photoLogsImageCardsEl.innerHTML = "";
-                insertHTML();
+            .then((res) => res.json())
+            .then((res) => {
+              for (var i = 0; i < res.length; i++) {
+                var URL = res[i].URL;
+                var id = res[i]._id;
+                var title = res[i].title;
+                var divImage = `<div class="cards mx-auto text-center col-4 col-lg-2" id=${id}>
+                                <p class="mt-4" data-id=${id}>
+                                  <p><img src=${URL} class="cardImage" /><br></p>
+                                  <span class="cardTitle">${title}&nbsp;<br>
+                                    <span onClick="delete" data-id=${id} class="delete">
+                                      <i class="far fa-trash-alt delete" data-id=${id}></i>
+                                    </span>
+                                  </span>
+                                </p>
+                              </div>`;
+
+                emptyHTML();
+                function emptyHTML() {
+                  photoLogsImageCardsEl.innerHTML = "";
+                  insertHTML();
+                }
+                function insertHTML() {
+                  photoLogsImageCardsEl.insertAdjacentHTML(
+                    "afterend",
+                    divImage
+                  );
+                }
               }
-              function insertHTML() {
-                photoLogsImageCardsEl.insertAdjacentHTML('afterend', divImage)
-              }
-            }
-          });
-        })
+            });
+        });
         var locationsAPI = API + divResponse;
         // fetch(locationsAPI)
         //   .then((res) => res.json())
