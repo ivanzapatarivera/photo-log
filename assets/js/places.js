271e47943b90b7eed94ed241129f78a480402929
Ivan Zapata <falenyo@gmail.com>
2020-11-12 23:39:58 -0500

    showing pics by location inside photo logs box OK

--- assets/js/places.js
+++ assets/js/places.js
@@ -23,12 +23,32 @@ function displayLocations() {
         // Display all images contained in each album
         var imageEl = document.getElementById(`${divResponse}`);
         var photoLogsBoxEl = document.getElementById('recentPhotosLogsBox');
-        var photoLogsImageCardsEl = document.getElementById('picturesWall')
+        var photoLogsImageCardsEl = document.getElementById('picturesWall');
+        var locationsAPI = API + divResponse;
+
         imageEl.addEventListener("click", function(){
-          console.log(`You've clicked ${divResponse}.`);
+          // console.log(`You've clicked ${divResponse}.`);
           photoLogsBoxEl.innerHTML = divResponse;
-          console.log(`You've clicked on ${photoLogsImageCardsEl.innerHTML}.`);
-          photoLogsImageCardsEl.style.backgroundColor = 'red';
+          // console.log(`You've clicked on ${photoLogsImageCardsEl.innerHTML}.`);
+          fetch(locationsAPI)
+          .then((res) => res.json())
+          .then((res) => {
+            console.log(res.length)
+            for (var i = 0; i < res.length; i++) {
+              var URL = res[i].URL;
+              // console.log(URL);
+              var divImage = `<a href="${URL}" target="_blank"><img src=${URL} class="col-10 col-md-3 my-2 divImage" /></a>`;
+              console.log(divImage);
+              emptyHTML();
+              function emptyHTML() {
+                photoLogsImageCardsEl.innerHTML = "";
+                insertHTML();
+              }
+              function insertHTML() {
+                photoLogsImageCardsEl.insertAdjacentHTML('afterend', divImage)
+              }
+            }
+          });
         })
         var locationsAPI = API + divResponse;
         // fetch(locationsAPI)
