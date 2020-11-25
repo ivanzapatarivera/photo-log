19eecbcd5bc04b7dc0046a03b5ad56089c5bc455
Ivan Zapata <falenyo@gmail.com>
2020-11-25 02:20:56 -0500

    formated JS code

--- assets/js/places.js
+++ assets/js/places.js
@@ -28,19 +28,19 @@ function displayLocations() {
 
         // Event listener to render image cards on dynamically generated card
         placesTraveledCityNameEl.addEventListener("click", () => {
-            placesEl.classList.remove("slide-in-top");
-            photoLogsBoxEl.innerHTML = divResponse;
-            photoLogsImageCardsEl.innerHTML = "";
-            fetch(locationsAPI)
-              .then((res) => res.json())
-              .then((res) => {
-                res.map((res) => {
-                  var URL = res.URL;
-                  var id = res._id;
-                  var title = res.title;
-                  var description = res.description;
+          placesEl.classList.remove("slide-in-top");
+          photoLogsBoxEl.innerHTML = divResponse;
+          photoLogsImageCardsEl.innerHTML = "";
+          fetch(locationsAPI)
+            .then((res) => res.json())
+            .then((res) => {
+              res.map((res) => {
+                var URL = res.URL;
+                var id = res._id;
+                var title = res.title;
+                var description = res.description;
 
-                  var card = `<div class="cards mx-auto text-center col-4 col-lg-2" id=${id}>
+                var card = `<div class="cards mx-auto text-center col-4 col-lg-2" id=${id}>
                                 <p class="mt-4" data-id=${id}>
                                   <p><img src=${URL} class="cardImage" /><br></p>
                                   <span class="cardTitle">${title}&nbsp;<br>
@@ -51,34 +51,34 @@ function displayLocations() {
                                 </p>
                               </div>`;
 
-                  photoLogsImageCardsEl.insertAdjacentHTML("afterbegin", card);
+                photoLogsImageCardsEl.insertAdjacentHTML("afterbegin", card);
 
-                  // Event to enlarge image when clicking on dynamically generated card
-                  const eventPictureClick = document.getElementById(id);
-                  eventPictureClick.addEventListener("click", (event) => {
-                    eventPicDiv.style.visibility = "visible";
-                    eventPicDiv.classList.add("flip-in-ver-left");
-                    var currentSrc = event.path[0].currentSrc;
-                    var enlargedImage = `<img src=${currentSrc} id=${currentSrc} class="col-12 col-md-10 enlargedImage">
+                // Event to enlarge image when clicking on dynamically generated card
+                const eventPictureClick = document.getElementById(id);
+                eventPictureClick.addEventListener("click", (event) => {
+                  eventPicDiv.style.visibility = "visible";
+                  eventPicDiv.classList.add("flip-in-ver-left");
+                  var currentSrc = event.path[0].currentSrc;
+                  var enlargedImage = `<img src=${currentSrc} id=${currentSrc} class="col-12 col-md-10 enlargedImage">
                                       <div id="caption" class="caption mt-0">${description}</div>`;
-                    if (eventPicDiv) {
-                      eventPicDiv.innerHTML = enlargedImage;
-                    }
+                  if (eventPicDiv) {
+                    eventPicDiv.innerHTML = enlargedImage;
+                  }
 
-                    // Event listener to change visbility of dynamically created image DIVs
-                    const eventCurrentSrc = document.getElementById(currentSrc);
-                    eventCurrentSrc.addEventListener("click", () => {
-                      if (eventPicDiv) {
-                        eventPicDiv.style.visibility = "hidden";
-                        eventPicDiv.classList.remove("flip-in-ver-left");
-                        eventPicDiv.innerHTML = "";
-                      }
-                    });
+                  // Event listener to change visbility of dynamically created image DIVs
+                  const eventCurrentSrc = document.getElementById(currentSrc);
+                  eventCurrentSrc.addEventListener("click", () => {
+                    if (eventPicDiv) {
+                      eventPicDiv.style.visibility = "hidden";
+                      eventPicDiv.classList.remove("flip-in-ver-left");
+                      eventPicDiv.innerHTML = "";
+                    }
                   });
                 });
               });
-            placesEl.style.visibility = "hidden";
-          });
+            });
+          placesEl.style.visibility = "hidden";
+        });
       });
     });
 }
