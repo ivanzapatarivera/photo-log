d39a3b4e08eb75672df104f6d611e0ca7a2b8d74
Ivan Zapata <falenyo@gmail.com>
2020-11-29 11:57:00 -0500

    updated names to match functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 import { nav } from "./navigation.js";
 import { logger } from "./logger.js";
-import { profile } from "./profile.js";
+import { profile } from "./profilepicture.js";
 import { status } from "./status.js";
 import { pictures } from "./pictures.js";
 import { places } from "./places.js";
