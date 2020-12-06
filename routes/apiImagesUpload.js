9cb87b846635cb6dd1e0b90557c49245bac57e35
Ivan Zapata <falenyo@gmail.com>
2020-12-05 23:08:06 -0500

    changed bucketname to PhotoLog

--- routes/apiImagesUpload.js
+++ routes/apiImagesUpload.js
@@ -11,7 +11,7 @@ let storage = new GridFsStorage({
     return new Promise((resolve, reject) => {
       const fileInfo = {
         filename: file.originalname,
-        bucketName: "imageUpload",
+        bucketName: "PhotoLog",
       };
       resolve(fileInfo);
     });
