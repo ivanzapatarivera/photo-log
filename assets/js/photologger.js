f869e1b31ed2e5ec13b9f4bacf7c349a257f026f
Ivan Zapata <falenyo@gmail.com>
2020-12-05 22:43:55 -0500

    form OK

--- assets/js/photologger.js
+++ assets/js/photologger.js
@@ -2,38 +2,47 @@ function photoLogger() {
   const body = document.querySelector("#root");
   const logNewPhotoDesktop = document.querySelector("#logNewPhoto");
   const logNewPhotoMobile = document.querySelector("#imagesMobile");
-  
+
   if (logNewPhotoDesktop) {
     readForm();
   }
 
   function readForm() {
     const logNewPhotoForm = `<div class="row mx-auto p-1 logNewPhotoForm" id="logNewPhotoFrame">
-                                    <div class="col-12 col-md-6 card mx-auto logNewPhotoFormDiv">
-                                        <div class="card-body">
-                                            <h5 class="text-center">Log your Photo!</h5>
-                                            <form action="/log" method="post">
-                                                <label for="title">Title</label><br>
-                                                <input class="px-3 py-1" type="text" name="title" id="title" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;How would you like to name this picture?"><br>
-                                                <label for="URL">Web Address</label><br>
-                                                <input class="px-3 py-1" type="text" name="URL" id="URL" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;What's the URL where this picture is located?"><br>
-                                                <label for="location">Location</label><br>
-                                                <input class="px-3 py-1" type="text" name="location" id="location" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Where was this picture taken?"><br>
-                                                <label for="description">Description</label><br>
-                                                <textarea class="px-3 py-1" type="text" name="description" id="description" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Would you like to say something about this picture?"></textarea><br>
-                                                <button class="button btn">Log</button>
-                                                <span class="button buttonCancel btn ml-1" id="logNewPhotoFormCancelBtn"><i class="fas fa-times"></i></span>
-                                            </form>
-                                            </div>
-                                            </div>
-                                            </div>`;
+                              <div class="col-12 col-md-6 card mx-auto logNewPhotoFormDiv">
+                                <div class="card-body">
+                                <h5 class="text-center">Log your Photo!</h5>
+                                <form action="/upload" method="post" enctype="multipart/form-data">
+                                  <label class="btn btn-primary">
+                                    <i class="fa fa-image"></i> Photo <input type="file" name="upload" id="upload" style="display: none;">
+                                  </label>
+                                  <div id="previewFileName"></div>
+                                  <button class="button btn" type="submit"><i class="fas fa-check"></i></button>
+                                  <span class="button buttonCancel btn ml-1" id="logNewPhotoFormCancelBtn"><i class="fas fa-times"></i></span>
+                                </form>     
+                                </div>
+                              </div>
+                            </div>`;
+
     body.insertAdjacentHTML("afterbegin", logNewPhotoForm);
+    var uploadBtn = document.getElementById("upload");
+    var previewFileName = document.getElementById("previewFileName");
+    uploadBtn.addEventListener("change", () => {
+      var uploadBtnArr = uploadBtn.value + "";
+      uploadBtnArr = uploadBtnArr.split("\\");
+      var i = uploadBtnArr.length - 1;
+      var fileName = uploadBtnArr[i];
+      console.log(uploadBtnArr);
+      console.log(fileName);
+      console.log(`You've selected this file: `, uploadBtn.value);
+      previewFileName.innerText = `Would you like to upload ${fileName} ?`;
+    });
     const logNewPhotoFrame = document.querySelector("#logNewPhotoFrame");
     const logNewPhotoFormCancelBtn = document.querySelector(
       "#logNewPhotoFormCancelBtn"
     );
     showHidePhotoForm(logNewPhotoForm, logNewPhotoFrame);
-    logNewPhotoFormCancel(logNewPhotoFrame, logNewPhotoFormCancelBtn);
+    logNewPhotoFormCancel(logNewPhotoFrame, logNewPhotoFormCancelBtn, previewFileName);
   }
 
   function showHidePhotoForm() {
@@ -60,6 +69,7 @@ function photoLogger() {
   function logNewPhotoFormCancel() {
     logNewPhotoFormCancelBtn.addEventListener("click", () => {
       logNewPhotoFrame.style.display = "none";
+      previewFileName.innerText = "";
     });
   }
 }
