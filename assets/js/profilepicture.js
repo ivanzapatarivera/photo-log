caffd6bae08b6d193c416b14e17088c9d04c857c
Ivan Zapata <falenyo@gmail.com>
2020-12-13 18:04:05 -0500

    removed profilePicture bg styling

--- assets/js/profilepicture.js
+++ assets/js/profilepicture.js
@@ -22,7 +22,6 @@ function profile() {
 
   // Display the latest posted image as profile picture
   const profilePicture = document.querySelector("#profilepic");
-  profilePicture.style.backgroundColor = "red";
   const profilePictureDiv = document.querySelector("#show");
   const profilePictureUpdateForm = document.querySelector("#newProfilePic");
   const profileAPI = "/profilepicturelog";
