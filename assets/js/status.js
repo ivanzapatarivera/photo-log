c5191ea651d74eb51ca424098e2d8b723fc3853e
Ivan Zapata <falenyo@gmail.com>
2020-12-22 11:27:36 -0500

    changed timestamp to temp lit

--- assets/js/status.js
+++ assets/js/status.js
@@ -12,13 +12,13 @@ function insertStatusForm() {
 
   // Manage eventListener's on statusForm
   statusFormEventListeners();
-  // Load previous status posts
-  loadPreviousStatusPosts();
+  // Render previous status posts
+  renderPreviousStatusPosts();
 }
 
 function statusFormHTML() {
   // Status Form Temp. Lit.
-  return `<div id="statusText" class="statusForm">
+  return `<div id="statusForm" class="statusForm">
             <form action="/status" method="post" id="formStatus" class="text-left">
               <textarea
                 name="statusUpdate"
@@ -56,34 +56,28 @@ function statusFormEventListeners(statusBtn, buttonSubmit, buttonCancel) {
 }
 
 
-function loadPreviousStatusPosts() {
+function renderPreviousStatusPosts(statusForm, previousStats, statusAPI) {
 
-  // This section will load all the previous status post
-  const status = document.querySelector("#statusText");
-  const previousStats = document.querySelector("#previousStatus");
-  const statusAPI = "/status";
+  statusForm = document.querySelector("#statusForm");
+  previousStats = document.querySelector("#previousStatus");
+  statusAPI = "/status";
 
-  if (status) {
-    previousStatus();
-    function previousStatus() {
+  if (statusForm) {
       fetch(statusAPI)
         .then((res) => res.json(statusAPI))
         .then((d) => {
+          console.log(d);
           d.map((d) => {
             let id = d._id;
             let date = d.timestamp;
             let dateArray = date.split("-");
-            let dateArray2 = dateArray[2].split("T");
-            let dateArray3 = dateArray2[1].split(".");
-            let timestamp =
-              "Created on: " +
-              dateArray[1] +
-              "/" +
-              dateArray2[0] +
-              "/" +
-              dateArray[0] +
-              " @" +
-              dateArray3[0];
+            let month = dateArray[1];
+            let dayTime = dateArray[2].split("T");
+            let time = dayTime[1].split(".");
+            time = time[0];
+            let day = dayTime[0];
+            let year = dateArray[0];
+            let timestamp = `Created on: ${month}/${day}/${year} @${time}`;
 
             let statusText = d.statusUpdate;
             let statusCard = `<div class="mt-3 col-12 col-md-12 mx-auto card text-left px-3 py-2 cardStatus" data-id="${id}">
@@ -101,7 +95,6 @@ function loadPreviousStatusPosts() {
         .catch((err) => {
           console.error("Error: ", err);
         });
-    }
     
 
     // This section will delete status post
