48c2f804671a96ff9baaec56ba1f3ab10fc519e7
Ivan Zapata <falenyo@gmail.com>
2020-12-22 11:54:09 -0500

    Month added to timestamp

--- assets/js/status.js
+++ assets/js/status.js
@@ -66,7 +66,6 @@ function renderPreviousStatusPosts(statusForm, previousStats, statusAPI) {
       fetch(statusAPI)
         .then((res) => res.json(statusAPI))
         .then((d) => {
-          console.log(d);
           d.map((d) => {
 
             let id = d._id;
@@ -79,24 +78,42 @@ function renderPreviousStatusPosts(statusForm, previousStats, statusAPI) {
             let day = dayTime[0];
             let year = dateArray[0];
 
-            // StatusCard timestamp
-            let timestamp = returnTimestamp(month, day, year, time);
 
-            function returnTimestamp(){
-              return `Created on: ${month}/${day}/${year} @${time}`
+            returnTimestamp(month, day, year, time);
+            // StatusCard timestamp
+            function returnTimestamp(){      
+
+              // 
+              if (month == 1) { month = 'January' };
+              if (month == 2) { month = 'February' };
+              if (month == 3) { month = 'March' };
+              if (month == 4) { month = 'April' };
+              if (month == 5) { month = 'May' };
+              if (month == 6) { month = 'June' };
+              if (month == 7) { month = 'July' };
+              if (month == 8) { month = 'August' };
+              if (month == 9) { month = 'September' };
+              if (month == 10) { month = 'October' };
+              if (month == 11) { month = 'November' };
+              if (month == 12) { month = 'December' };
+              
+              let timestamp =  `Created on: ${month} ${day}, ${year} @${time}`
+              
+
+              let statusText = d.statusUpdate;
+              let statusCard = `<div class="mt-3 col-12 col-md-12 mx-auto card text-left px-3 py-2 cardStatus" data-id="${id}">
+                                <span style="font-size: .8rem;">${timestamp} 
+                                  <span onClick="delete" class="delete">
+                                    <i class="far fa-trash-alt text-right delete" data-id=${id}></i>
+                                  </span>
+                                </span>
+                                <p class="statusText pt-2" data-id="${id}">${statusText}</p>
+                              </div>`;
+  
+              previousStats.insertAdjacentHTML("afterbegin", statusCard);
+
             }
 
-            let statusText = d.statusUpdate;
-            let statusCard = `<div class="mt-3 col-12 col-md-12 mx-auto card text-left px-3 py-2 cardStatus" data-id="${id}">
-                              <span style="font-size: .8rem;">${timestamp} 
-                                <span onClick="delete" class="delete">
-                                  <i class="far fa-trash-alt text-right delete" data-id=${id}></i>
-                                </span>
-                              </span>
-                              <p class="statusText pt-2" data-id="${id}">${statusText}</p>
-                            </div>`;
-
-            previousStats.insertAdjacentHTML("afterbegin", statusCard);
           });
         })
         .catch((err) => {
